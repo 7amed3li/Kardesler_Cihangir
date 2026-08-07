c6f88d66c7d213cd53496dd37e5967c703cf0120
Hamed Mohamed <hamedmohamed22w@gmail.com>
2026-08-07 15:45:05 +0300

    feat: Set Saudi Riyal display to ر.س SAR

--- src/components/CurrencySwitcher.jsx
+++ src/components/CurrencySwitcher.jsx
@@ -14,7 +14,7 @@ export default function CurrencySwitcher() {
     { code: "USD", symbol: "$" },
     { code: "EUR", symbol: "€" },
     { code: "GBP", symbol: "£" },
-    { code: "SAR", symbol: "﷼" },
+    { code: "SAR", symbol: "ر.س" },
     { code: "RUB", symbol: "₽" },
   ];
 
